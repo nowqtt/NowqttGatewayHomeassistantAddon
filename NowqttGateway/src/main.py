d45ebe985179db2c1b13dda072605dbb17208db3
ChrisC132 <c.costa44@gmx.de>
2024-04-08 21:25:44 +0200

    feat: Added format and timestamp to commit

--- NowqttGateway/src/main.py
+++ NowqttGateway/src/main.py
@@ -21,7 +21,11 @@ if __name__ == '__main__':
         except Exception as e:
             logging.error(e)
 
-    logging.basicConfig(level=global_vars.config["log_level"])
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=global_vars.config["log_level"],
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
     com_port = global_vars.config["serial"]["com_port"]
     baudrate = global_vars.config["serial"]["baudrate"]
