fce9f8e5b54abaf9be928214e070c2669cc8e1f5
ChrisC132 <c.costa44@gmx.de>
2024-05-24 08:40:43 +0200

    feat: added redirect on home page

--- NowqttGateway/src/web_server.py
+++ NowqttGateway/src/web_server.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from flasgger import Swagger
 
 import global_vars
@@ -15,14 +15,39 @@ swagger = Swagger(app, template_file=os.path.abspath('/app/spec/swagger.yaml'))
 
 @app.route("/v1/devices", methods=['GET'], endpoint='devices')
 def devices():
-    return db_helper.fetch_devices()
+    return Response(response=db_helper.fetch_devices(),
+                    status=200,
+                    mimetype="application/json")
 
 @app.route("/v1/traces", methods=['GET'], endpoint='traces')
 def traces():
     device_mac_address = request.args.get('device_mac_address', default=None, type=str)
     last = request.args.get('last', default=None, type=str)
 
-    return db_helper.fetch_traces(device_mac_address, last)
+    return Response(response=db_helper.fetch_traces(device_mac_address, last),
+                        status=200,
+                        mimetype="application/json")
+
+@app.route("/", methods=['GET'])
+def home():
+    response_body = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Welcome to the API</title>
+        </head>
+        <body>
+            <h1>Welcome to the Nowqtt Gateway</h1>
+            <p>You can find the API documentation <a href="/apidocs">here</a>.</p>
+        </body>
+        </html>
+    """
+
+    return Response(response=response_body,
+                        status=200,
+                        mimetype="text/html")
 
 def run():
     logging.info("Web server running on port %s", global_vars.config["web_server"]["port"])
